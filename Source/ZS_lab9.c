d6f121fd486ad6776701a9695ff389157a59dad6
Ziyad Mekhemer <zmek95@gmail.com>
2020-03-26 17:30:23 -0400

    Add files via upload

--- Source/ZS_lab9.c
+++ Source/ZS_lab9.c
@@ -18,70 +18,70 @@ void speedProfile(uint16_t channel,int newDutyCycleFlag);
 //global variables
 uint32_t counter[2]	  = {0,0};
 uint8_t  timerDone[2] = {1,1};
-float increment[2] = {0,0}; 
+float increment[2] = {0,0};//used to store the current speed for the speedProfile function
 extern struct queue* front1;
 extern struct queue* front2;
 
 void DCinit(void){
-	GPIO_InitTypeDef  GPIO_InitStruct = { 0 };
-	TIM_OC_InitTypeDef  sConfig;
-	TIM_HandleTypeDef tim1;
-	// Enabling clock for GPIOs
-	__HAL_RCC_GPIOA_CLK_ENABLE();
-	__HAL_RCC_GPIOB_CLK_ENABLE();
-	//configure GPIO pins for Motor1
-	GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-	GPIO_InitStruct.Alternate = 0;
-	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-	//configure GPIO pins for Motor2
-	GPIO_InitStruct.Pin = GPIO_PIN_10 | GPIO_PIN_11;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-	GPIO_InitStruct.Alternate = 0;
-	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-	//configuring GPIO pins for PWM outputs
-	GPIO_InitStruct.Pin = GPIO_PIN_8 | GPIO_PIN_9;
-	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-	GPIO_InitStruct.Alternate = 6;
-	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-	//Making sure motors are not activated
-	motorStop(1);
-	motorStop(2);
-	//configuring TIM1
-	__HAL_RCC_TIM1_CLK_ENABLE();
-	tim1.Instance = TIM1;
-	tim1.Init.Prescaler = HAL_RCC_GetPCLK2Freq() / 1000000 - 1;
-	tim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-	tim1.Init.Period = 1000;
-	tim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-	tim1.Init.RepetitionCounter = 0;
-	TIM1 -> CR1 |= (TIM_CR1_URS); //only counter over/under flow generates interrupt
-	TIM1 -> DIER |= 0b1; //setting Update Interrupt Enable bit 
-	TIM1 -> EGR |= 0b1; //setting Update Generation bit 
-	HAL_TIM_Base_Init(&tim1);
-  	//Configuring PWM
-	sConfig.OCMode = TIM_OCMODE_PWM1;
-	sConfig.Pulse = 0;
-	sConfig.OCPolarity = TIM_OCPOLARITY_HIGH;
-	sConfig.OCNPolarity = TIM_OCNPOLARITY_LOW;
-	sConfig.OCFastMode = TIM_OCFAST_DISABLE;
-	sConfig.OCIdleState = TIM_OCIDLESTATE_RESET;
-	sConfig.OCNIdleState = TIM_OCNIDLESTATE_RESET;
-	HAL_TIM_PWM_ConfigChannel(&tim1, &sConfig, TIM_CHANNEL_1);
-	HAL_TIM_PWM_Start(&tim1, TIM_CHANNEL_1);
-	HAL_TIM_PWM_ConfigChannel(&tim1, &sConfig, TIM_CHANNEL_2);
-	HAL_TIM_PWM_Start(&tim1, TIM_CHANNEL_2);
-	 //Enable Interrupts 
-	HAL_NVIC_SetPriority(TIM1_UP_TIM16_IRQn, 0, 1);//set to position 0 and priority 1
-	NVIC_EnableIRQ(TIM1_UP_TIM16_IRQn);
-  	HAL_NVIC_SetPriority(TIM1_CC_IRQn, 1, 2);//set to position 0 and priority 1
-  	NVIC_EnableIRQ(TIM1_CC_IRQn);
+  GPIO_InitTypeDef  GPIO_InitStruct = { 0 };
+  TIM_OC_InitTypeDef  sConfig;
+  TIM_HandleTypeDef tim1;
+  // Enabling clock for GPIOs
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  //configure GPIO pins for Motor1
+  GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.Alternate = 0;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  //configure GPIO pins for Motor2
+  GPIO_InitStruct.Pin = GPIO_PIN_10 | GPIO_PIN_11;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.Alternate = 0;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  //configuring GPIO pins for PWM outputs
+  GPIO_InitStruct.Pin = GPIO_PIN_8 | GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.Alternate = 6;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  //Making sure motors are not activated
+  motorStop(1);
+  motorStop(2);
+  //configuring TIM1
+  __HAL_RCC_TIM1_CLK_ENABLE();
+  tim1.Instance = TIM1;
+  tim1.Init.Prescaler = HAL_RCC_GetPCLK2Freq() / 1000000 - 1;
+  tim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  tim1.Init.Period = 1000;
+  tim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  tim1.Init.RepetitionCounter = 0;
+  TIM1 -> CR1 |= (TIM_CR1_URS); //only counter over/under flow generates interrupt
+  TIM1 -> DIER |= 0b1; //setting Update Interrupt Enable bit 
+  TIM1 -> EGR |= 0b1; //setting Update Generation bit 
+  HAL_TIM_Base_Init(&tim1);
+  //Configuring PWM
+  sConfig.OCMode = TIM_OCMODE_PWM1;
+  sConfig.Pulse = 0;
+  sConfig.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfig.OCNPolarity = TIM_OCNPOLARITY_LOW;
+  sConfig.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfig.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfig.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  HAL_TIM_PWM_ConfigChannel(&tim1, &sConfig, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&tim1, TIM_CHANNEL_1);
+  HAL_TIM_PWM_ConfigChannel(&tim1, &sConfig, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&tim1, TIM_CHANNEL_2);
+  //Enable Interrupts 
+  HAL_NVIC_SetPriority(TIM1_UP_TIM16_IRQn, 0, 1);//set to position 0 and priority 1
+  NVIC_EnableIRQ(TIM1_UP_TIM16_IRQn);
+  HAL_NVIC_SetPriority(TIM1_CC_IRQn, 1, 2);
+  NVIC_EnableIRQ(TIM1_CC_IRQn);
 }
 
 void motorStop(uint16_t channel){
@@ -135,14 +135,19 @@ void DC(uint16_t channel, uint16_t dutyCycle, uint16_t direction){
   TIM1->CR1 |= TIM_CR1_CEN;	//resuming timer
 }
 
+// FUNCTION      : speedProfile
+// DESCRIPTION   : This function applies a speed profile based on a waveform that is similar to an S-waveform for a specified duty cycle
+// PARAMETERS    : uint16_t channel - 2 channels that can generate PWM waveforms independently
+//                 int newDutyCycleFlag - This flag is for checking whether a new duty cycle has been entered by the user
+// RETURNS       : Nothing
 void speedProfile(uint16_t channel,int newDutyCycleFlag){
 	
   float y;//On startup CCR1 and CCR2 registers must be 0, y is the speed of the DC motor
   float x;//represents the increment in x for the function -140((x-1)^3)*x^3 
-  //-140((x-1)^3)*x^3 is the derivative of a S3 SMOOTHSTEP function 
+          //-140((x-1)^3)*x^3 is the derivative of a S3 SMOOTHSTEP function 
   int newSpeed;
 	
-  //This block is for varying the motor speed before it has come to a stop.
+  //This block is for varying the motor speed before it has come to a stop anad a new duty cycle has been entered.
   if (newDutyCycleFlag == 1){
     if (channel == 1){
       TIM1->DIER |= TIM_DIER_CC1IE;
@@ -172,7 +177,7 @@ void speedProfile(uint16_t channel,int newDutyCycleFlag){
     }
   }
 	
-//This block contains the calculations for the speed profile
+  //This block contains the calculations for the speed profile
   if (channel == 1){
     if (counter[0] == 1000){//Start slowing down the motor when counter is at 1000
       if(increment[0] < 0.5){
@@ -233,55 +238,55 @@ ADD_CMD("DCInit", CmdDCInit, "	      Initializes DC motor driver, TIM1 with PWM 
 
 ParserReturnVal_t CmdDC(int mode) {
 
-	uint16_t channel = 0;  //used to choose which motor to be activated
-	uint16_t dutyCycle = 0;//used to control speed of motor
-	uint16_t direction = 0;//used to set direction of rotation
-	uint32_t time = 0;	   //time in milliseconds
-	uint16_t rc;
+  uint16_t channel = 0;  //used to choose which motor to be activated
+  uint16_t dutyCycle = 0;//used to control speed of motor
+  uint16_t direction = 0;//used to set direction of rotation
+  uint32_t time = 0;	   //time in milliseconds
+  uint16_t rc;
 
   if (mode != CMD_INTERACTIVE) return CmdReturnOk;
 
 
-	rc = fetch_uint16_arg(&channel);	//inputting channel number from user
-	if (rc) {
-		printf("Please select which motor to control\n");
-		return CmdReturnBadParameter1;
-	}
-	rc = fetch_uint16_arg(&direction);	//inputting direction number from user
-	if (rc) {
-		printf("Please select direction of rotation\n");
-		return CmdReturnBadParameter3;
-	}
-	if(direction == 1 || direction == 2){
-		rc = fetch_uint32_arg(&time);	//inputting direction number from user
-		if (rc) {
-			printf("Please enter time in milliseconds\n");
-			return CmdReturnBadParameter4;
-		}
-		rc = fetch_uint16_arg(&dutyCycle);	//inputting speed from user
-		if (rc) {
-			printf("Please enter the speed, (0 to 100)\n");
-			return CmdReturnBadParameter2;
-		}
-	} 
-	if (channel > 2 || channel < 1) {	//checking if channel number is valid 
-		printf("Type either 1 or 2 for motor selection\n");
-		return CmdReturnBadParameter1;
-	}
-	if (direction > 2 || direction < 0) {	//checking if direction is valid 
-		printf("Type 1 for CW, 2 for CCW or 0 to stop motor\n");
-		return CmdReturnBadParameter3;
-	}
-	if (time < 0 || time > 30000) {	//checking if time is valid 
-		printf("time can't be negative or greater than 30000 ms\n");
-		return CmdReturnBadParameter4;
-	}
-	if (dutyCycle > 100 || dutyCycle < 0) {	//checking if speed selected is valid 
-		printf("speed must be between 0 and 100\n");
-		return CmdReturnBadParameter2;
-	}
-	addToQueue(channel, dutyCycle, direction, time);
-	return CmdReturnOk;
+  rc = fetch_uint16_arg(&channel);	//inputting channel number from user
+  if (rc) {
+    printf("Please select which motor to control\n");
+    return CmdReturnBadParameter1;
+  }
+  rc = fetch_uint16_arg(&direction);	//inputting direction number from user
+  if (rc) {
+    printf("Please select direction of rotation\n");
+    return CmdReturnBadParameter3;
+  }
+  if(direction == 1 || direction == 2){
+    rc = fetch_uint32_arg(&time);	//inputting direction number from user
+    if (rc) {
+      printf("Please enter time in milliseconds\n");
+      return CmdReturnBadParameter4;
+    }
+    rc = fetch_uint16_arg(&dutyCycle);	//inputting speed from user
+    if (rc) {
+      printf("Please enter the speed, (0 to 100)\n");
+      return CmdReturnBadParameter2;
+    }
+  } 
+  if (channel > 2 || channel < 1) {	//checking if channel number is valid 
+    printf("Type either 1 or 2 for motor selection\n");
+    return CmdReturnBadParameter1;
+  }
+  if (direction > 2 || direction < 0) {	//checking if direction is valid 
+    printf("Type 1 for CW, 2 for CCW or 0 to stop motor\n");
+    return CmdReturnBadParameter3;
+  }
+  if (time < 0 || time > 30000) {	//checking if time is valid 
+    printf("time can't be negative or greater than 30000 ms\n");
+    return CmdReturnBadParameter4;
+  }
+  if (dutyCycle > 100 || dutyCycle < 0) {	//checking if speed selected is valid 
+    printf("speed must be between 0 and 100\n");
+    return CmdReturnBadParameter2;
+  }
+  addToQueue(channel, dutyCycle, direction, time);
+  return CmdReturnOk;
 }
 ADD_CMD("DC", CmdDC, "<channel><direction><time><speed>  activates selected motor and sets direction, speed and operating time if stop is requested")
 /***************ISR***************/
@@ -291,28 +296,28 @@ ADD_CMD("DC", CmdDC, "<channel><direction><time><speed>  activates selected moto
 // RETURNS       : Nothing
 void TIM1_UP_TIM16_IRQHandler(void) {
 	
-	TIM1->CR1 &= ~TIM_CR1_CEN; 	//stopping timer
+  TIM1->CR1 &= ~TIM_CR1_CEN; 	//stopping timer
 	
-	if(counter[0] == 0){//checking if channel 1 timed-out
-		motorStop(1);	//stopping motor 1
-		TIM1->CCER &= 0xFFFFFFFE;	//disabling channel1 output
-		TIM1->CCER |= 0x04;
-		timerDone[0] = 1;
-	}
-	else{
-		counter[0]--;
-	}	
-	if(counter[1] == 0){//checking if channel 2 timed-out
-		motorStop(2);	//stopping motor 2
-		TIM1->CCER &= 0xFFFFFFEF;	//disabling channel2 output
-		TIM1->CCER |= 0x40;
-		timerDone[1] = 1;
-	}
-	else{
-		counter[1]--;
-	}
-	TIM1->CR1 |= TIM_CR1_CEN;	//resuming timer
-	TIM1 -> SR &= 0xfffe;// Resetting the Update Interrupt Flag (UIF) to 0
+  if(counter[0] == 0){//checking if channel 1 timed-out
+    motorStop(1);	//stopping motor 1
+    TIM1->CCER &= 0xFFFFFFFE;	//disabling channel1 output
+    TIM1->CCER |= 0x04;
+    timerDone[0] = 1;
+  }
+  else{
+    counter[0]--;
+  }	
+  if(counter[1] == 0){//checking if channel 2 timed-out
+    motorStop(2);	//stopping motor 2
+    TIM1->CCER &= 0xFFFFFFEF;	//disabling channel2 output
+    TIM1->CCER |= 0x40;
+    timerDone[1] = 1;
+  }
+  else{
+    counter[1]--;
+  }
+  TIM1->CR1 |= TIM_CR1_CEN;	//resuming timer
+  TIM1 -> SR &= 0xfffe;// Resetting the Update Interrupt Flag (UIF) to 0
 
 }
 
@@ -327,11 +332,11 @@ void TIM1_UP_TIM16_IRQHandler(void) {
 //quantify 2.1875 into 500 points
 
 // FUNCTION      : TIM1_CC_IRQHandler
-// DESCRIPTION   : Interrupt for controlling the PWM duty cycle for BreathingPWM for the TIM1 CC register.
+// DESCRIPTION   : Interrupt that executes the speedProfile function everytime the counter compare register matches the counter.
 // PARAMETERS    : Nothing
 // RETURNS       : Nothing
 void TIM1_CC_IRQHandler(void) {
-  //need user entered data
+
   TIM1->CR1 &= ~TIM_CR1_CEN; 	//stopping timer
   //checking interrupt flags
   if (TIM1->SR & TIM_SR_CC1IF) {	//Capture Compare register 1
@@ -341,14 +346,6 @@ void TIM1_CC_IRQHandler(void) {
   if (TIM1->SR & TIM_SR_CC2IF) {	//Capture Compare register 2
     speedProfile(2,0);
     TIM1->SR &= ~TIM_SR_CC2IF;// Resetting the CC2 Interrupt Flag to 0
-  }
-	
-	
-  //updating duty cycle on breathing profile function based on mode selected 
-  //put DC function here and pass it the duty cycle from function.
-	
-  //If statement here for when the desired speed is reached to skip following block
-	
-	
+  }	
   TIM1->CR1 |= TIM_CR1_CEN;	//starting timer
 }
