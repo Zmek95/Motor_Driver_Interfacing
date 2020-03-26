2ca31d7fe6e3e122b1ece67c00c748e024081612
Shamseddin Elmasri <61571666+shamseddinElmasri@users.noreply.github.com>
2020-03-26 03:28:54 -0400

    Add files via upload

--- Source/ZS_lab9.c
+++ Source/ZS_lab9.c
@@ -9,7 +9,7 @@
 #include "common.h"
 #include "DC_CommandQueue.h"
 //prototypes
-void PWMInit(void);
+//void PWMInit(void);
 void DCinit(void);
 void motorStop(uint16_t channel);
 void DC(uint16_t channel, uint16_t dutyCycle, uint16_t direction);
@@ -39,6 +39,13 @@ void DCinit(void){
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
 	GPIO_InitStruct.Alternate = 0;
 	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+	//configuring GPIO pins for PWM outputs
+	GPIO_InitStruct.Pin = GPIO_PIN_8 | GPIO_PIN_9;
+	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+	GPIO_InitStruct.Alternate = 6;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 	//Making sure motors are not activated
 	motorStop(1);
 	motorStop(2);
@@ -62,7 +69,9 @@ void DCinit(void){
 	sConfig.OCIdleState = TIM_OCIDLESTATE_RESET;
 	sConfig.OCNIdleState = TIM_OCNIDLESTATE_RESET;
 	HAL_TIM_PWM_ConfigChannel(&tim1, &sConfig, TIM_CHANNEL_1);
+	HAL_TIM_PWM_Start(&tim1, TIM_CHANNEL_1);
 	HAL_TIM_PWM_ConfigChannel(&tim1, &sConfig, TIM_CHANNEL_2);
+	HAL_TIM_PWM_Start(&tim1, TIM_CHANNEL_2);
 	 //Enable Interrupts 
 	HAL_NVIC_SetPriority(TIM1_UP_TIM16_IRQn, 0, 1);//set to position 0 and priority 1
 	NVIC_EnableIRQ(TIM1_UP_TIM16_IRQn);
@@ -85,10 +94,13 @@ void motorStop(uint16_t channel){
 //				   uint16_t direction - direction of rotation
 // RETURNS       : Nothing
 void DC(uint16_t channel, uint16_t dutyCycle, uint16_t direction){
+	
 	dutyCycle = dutyCycle * 10;	//scaling up to 1000 microseconds per overflow
 	TIM1->CR1 &= ~TIM_CR1_CEN;	//stopping timer
 	if (channel == 1) {
 		TIM1->CCR1 = dutyCycle;
+		TIM1->CCER &= 0xFFFFFFFC;	//enabling channel1 output
+		TIM1->CCER |= 0x01;
 		if(direction == 1){
 			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, 1);
 			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, 0);
@@ -99,6 +111,9 @@ void DC(uint16_t channel, uint16_t dutyCycle, uint16_t direction){
 	}
 	else{
 		TIM1->CCR2 = dutyCycle;
+		TIM1->CCER &= 0xFFFFFFCF;	//enabling channel2 output
+		TIM1->CCER |= 0x10;
+
 		if(direction == 1){
 			if(direction == 1){
 			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, 1);
@@ -123,10 +138,10 @@ ADD_CMD("DCInit", CmdDCInit, "	      Initializes DC motor driver, TIM1 with PWM 
 
 ParserReturnVal_t CmdDC(int mode) {
 
-	uint16_t channel;  //used to choose which motor to be activated
-	uint16_t dutyCycle;//used to control speed of motor
-	uint16_t direction;//used to set direction of rotation
-	uint16_t time;	   //time in milliseconds
+	uint16_t channel = 0;  //used to choose which motor to be activated
+	uint16_t dutyCycle = 0;//used to control speed of motor
+	uint16_t direction = 0;//used to set direction of rotation
+	uint32_t time = 0;	   //time in milliseconds
 	uint16_t rc;
 
 	if (mode != CMD_INTERACTIVE) return CmdReturnOk;
@@ -136,27 +151,15 @@ ParserReturnVal_t CmdDC(int mode) {
 		printf("Please select which motor to control\n");
 		return CmdReturnBadParameter1;
 	}
-	if (channel > 2 || channel < 1) {	//checking if channel number is valid 
-		printf("Type either 1 or 2 for motor selection\n");
-		return CmdReturnBadParameter1;
-	}
 	rc = fetch_uint16_arg(&direction);	//inputting direction number from user
 	if (rc) {
 		printf("Please select direction of rotation\n");
 		return CmdReturnBadParameter3;
 	}
-	if (direction > 2 || direction < 0) {	//checking if direction is valid 
-		printf("Type 1 for CW, 2 for CCW, 0 or 3 to stop motor\n");
-		return CmdReturnBadParameter3;
-	}
 	if(direction == 1 || direction == 2){
-		rc = fetch_uint16_arg(&time);	//inputting direction number from user
+		rc = fetch_uint32_arg(&time);	//inputting direction number from user
 		if (rc) {
 			printf("Please enter time in milliseconds\n");
-			return CmdReturnBadParameter4;
-		}
-		if (time < 0 || time > 30000) {	//checking if time is valid 
-			printf("time can't be negative or greater than 30000 ms\n");
 			return CmdReturnBadParameter4;
 		}
 		rc = fetch_uint16_arg(&dutyCycle);	//inputting speed from user
@@ -164,32 +167,50 @@ ParserReturnVal_t CmdDC(int mode) {
 			printf("Please enter the speed, (0 to 100)\n");
 			return CmdReturnBadParameter2;
 		}
-		if (dutyCycle > 100 || dutyCycle < 0) {	//checking if speed selected is valid 
-			printf("speed must be between 0 and 100\n");
-			return CmdReturnBadParameter2;
-		}
+	} 
+	if (channel > 2 || channel < 1) {	//checking if channel number is valid 
+		printf("Type either 1 or 2 for motor selection\n");
+		return CmdReturnBadParameter1;
 	}
-	//DC(channel, dutyCycle, direction, time);
+	if (direction > 2 || direction < 0) {	//checking if direction is valid 
+		printf("Type 1 for CW, 2 for CCW or 0 to stop motor\n");
+		return CmdReturnBadParameter3;
+	}
+	if (time < 0 || time > 30000) {	//checking if time is valid 
+		printf("time can't be negative or greater than 30000 ms\n");
+		return CmdReturnBadParameter4;
+	}
+	if (dutyCycle > 100 || dutyCycle < 0) {	//checking if speed selected is valid 
+		printf("speed must be between 0 and 100\n");
+		return CmdReturnBadParameter2;
+	}
 	addToQueue(channel, dutyCycle, direction, time);
 	return CmdReturnOk;
 }
-ADD_CMD("DC", CmdDC, "<channel><direction><speed><time>  activates selected motor and sets direction, speed and operating time if stop is requested")
+ADD_CMD("DC", CmdDC, "<channel><direction><time><speed>  activates selected motor and sets direction, speed and operating time if stop is requested")
 /***************ISR***************/
 // FUNCTION      : TIM1_UP_IRQHandler
 // DESCRIPTION   : 
 // PARAMETERS    : Nothing
 // RETURNS       : Nothing
 void TIM1_UP_TIM16_IRQHandler(void) {
-
+	
 	TIM1->CR1 &= ~TIM_CR1_CEN; 	//stopping timer
+	
 	if(counter[0] == 0){//checking if channel 1 timed-out
 		motorStop(1);	//stopping motor 1
+		TIM1->CCER &= 0xFFFFFFFE;	//disabling channel1 output
+		TIM1->CCER |= 0x04;
+		timerDone[0] = 1;
 	}
 	else{
 		counter[0]--;
 	}	
 	if(counter[1] == 0){//checking if channel 2 timed-out
 		motorStop(2);	//stopping motor 2
+		TIM1->CCER &= 0xFFFFFFEF;	//disabling channel2 output
+		TIM1->CCER |= 0x40;
+		timerDone[1] = 1;
 	}
 	else{
 		counter[1]--;
