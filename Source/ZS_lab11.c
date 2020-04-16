e8637bf2ec624eaec8b7bc9cbc94ccc7003439c5
Shamseddin Elmasri <61571666+shamseddinElmasri@users.noreply.github.com>
2020-04-16 04:09:11 -0400

    Update ZS_lab11.c

--- Source/ZS_lab11.c
+++ Source/ZS_lab11.c
@@ -6,10 +6,12 @@
 #include <stdint.h>
 #include "common.h"
 
+//TypeDefs
 TIM_HandleTypeDef tim1;
 TIM_OC_InitTypeDef  sConfig;
 GPIO_InitTypeDef  GPIO_InitStruct = { 0 };
 ADC_HandleTypeDef hadc;
+
 //prototypes
 void PWMInit(void);
 void adcInit(void);
@@ -17,13 +19,86 @@ void HD44780_Init(void);
 void HD44780_GotoXY(unsigned char x, unsigned char y);
 void HD44780_PutStr(char *str);
 void HD44780_ClrScr(void);
+
 //global variables
 char stringBuffer[16]= {0};     //buffer used to store different datatypes for LCD
-volatile uint16_t controlMeasuredSpeed = 0;
-uint16_t desiredSpeed = 0;
-int Kp = 6;			//proportional constant
-int32_t errorValue;	// to be put back as local after debugging complete
-int32_t testNewValue;   //for debugging purpose only
+volatile uint32_t controlMeasuredSpeed = 0; //used to hold the measured output value
+uint32_t desiredSpeed = 0;
+int Kp = 1;			//proportional constant
+int Kd = 4;			//differential constant
+int32_t proportionalError = 0;
+int32_t differentialError = 0;
+int32_t previousError = 0;
+/*************Commands****************************/
+
+ParserReturnVal_t CmdSetInput(int mode) {
+
+  if (mode != CMD_INTERACTIVE) return CmdReturnOk;
+  fetch_uint32_arg(&desiredSpeed);		//inputting desired speed
+  TIM1->CR1 &= ~TIM_CR1_CEN;    		//stopping timer	
+  TIM1->CCR1 = desiredSpeed * 65500 / 1738;	//setting desired speed as input
+  TIM1->CR1 |= TIM_CR1_CEN;			//resuming timer
+  HAL_ADC_Start_IT(&hadc);			//start ADC
+  return CmdReturnOk;
+}
+ADD_CMD("SetInput", CmdSetInput, "		Sets desired speed 0 - 1738")
+
+/*******************Tasks**************************/
+void controlTask(void *data){
+	
+  static int LCDDelay = 0;
+  uint16_t outputPercentage;
+
+    HAL_ADC_Start_IT(&hadc); //taking new sample from output 
+
+  //display results on LCD every 750 ms
+  if(LCDDelay % 75 == 0){
+    outputPercentage = controlMeasuredSpeed * 100 / desiredSpeed ; //calculating percentage of output with respect to desired set point
+    //displaying on LCD line 1
+    HD44780_GotoXY(0,0);	
+    sprintf(stringBuffer,"%03d prcnt stp",outputPercentage);
+    HD44780_PutStr(stringBuffer);
+    //displaying current duty cycle on LCD line 2      	                
+    HD44780_GotoXY(0,1);	
+    sprintf(stringBuffer,"@ %03lu dutyCyc",TIM1->CCR1 * 100 / 65500);
+    HD44780_PutStr(stringBuffer);			
+  }
+  LCDDelay++;
+}
+ADD_TASK(controlTask, controlInit, NULL, 10, "DC motor control task")
+
+
+/***************************ISR****************************/
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc1)
+    {   
+	int32_t newValue = 0; //used to update the duty cycle
+	int32_t errorValue = 0;
+
+	if(hadc1->Instance == ADC1){
+	  //PID controller code
+  	  controlMeasuredSpeed = HAL_ADC_GetValue(&hadc);   //copying result of conversion
+	  errorValue = desiredSpeed - controlMeasuredSpeed; //error = input - output
+	  proportionalError = errorValue;
+	  differentialError = errorValue - previousError;
+	  previousError = errorValue;	//storing current error for next sample's previous error
+	  newValue = (proportionalError * Kp) + (differentialError * Kd) + TIM1->CCR1 * 1738 / 65500;
+	  
+	  //setting limits for the new value
+	  if(newValue > 1738){
+	    newValue = 1738;
+	  }else if(newValue < 0){
+	    newValue = 0;
+	  }
+	    TIM1->CR1 &= ~TIM_CR1_CEN;  	  //stopping timer	
+	    TIM1->CCR1 =  newValue * 65500 / 1738;//updating duty cyle
+	    TIM1->CR1 |= TIM_CR1_CEN;	          //resuming timer
+	}
+    }
+void ADC1_IRQHandler(){
+	HAL_ADC_IRQHandler(&hadc); 
+}
+
 /******************Functions****************************/
 void controlInit(void *data){
   
@@ -40,7 +115,6 @@ void controlInit(void *data){
   GPIO_InitStruct.Alternate = 6;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
   tim1.Instance = TIM1;
- // tim1.Init.Prescaler = HAL_RCC_GetPCLK2Freq() / 1000000 - 1;
   tim1.Init.Prescaler = 35;
   tim1.Init.CounterMode = TIM_COUNTERMODE_UP;
   tim1.Init.Period = 65500;
@@ -48,7 +122,7 @@ void controlInit(void *data){
   tim1.Init.RepetitionCounter = 0;
   HAL_TIM_Base_Init(&tim1);
   sConfig.OCMode = TIM_OCMODE_PWM1;
-  sConfig.Pulse = 10;
+  sConfig.Pulse = 0;
   sConfig.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfig.OCNPolarity = TIM_OCNPOLARITY_LOW;
   sConfig.OCFastMode = TIM_OCFAST_DISABLE;
@@ -78,7 +152,7 @@ void controlInit(void *data){
   hadc.Init.ExternalTrigConvEdge = 0;
   hadc.Init.DMAContinuousRequests = DISABLE;
   hadc.Init.Overrun = ADC_OVR_DATA_OVERWRITTEN;
-  HAL_ADC_Init(&hadc);
+  HAL_ADC_Init(&hadc);				//initializing ADC
   ADC_ChannelConfTypeDef config;
   config.Channel = 6;
   config.Rank = 1;
@@ -88,92 +162,11 @@ void controlInit(void *data){
   config.Offset = 0;
   HAL_ADC_ConfigChannel(&hadc, &config);
 
-  HAL_NVIC_SetPriority(ADC1_IRQn, 2, 3);
-  HAL_NVIC_EnableIRQ(ADC1_IRQn);
+  HAL_NVIC_SetPriority(ADC1_IRQn, 2, 3);	
+  HAL_NVIC_EnableIRQ(ADC1_IRQn);		
   
   ADC1->IER |= 0x4; //enable end of regular conversion interrupt setting EOCIE bit high
   ADC1->CR |= 1;  //enabling ADC
   
-  //initializing LCD
-  HD44780_Init();	
-}
-
-/*******************Tasks**************************/
-void controlTask(void *data){
-	
-  static int LCDDelay = 0;
-  uint16_t outputPercentage;
-
-    HAL_ADC_Start_IT(&hadc);  
-
-  if(LCDDelay % 25 == 0){
-    printf("MeasuredSpeed = %u\terror = %ld\tnewValue = %ld\n ", controlMeasuredSpeed, errorValue/Kp, testNewValue);
-    outputPercentage = controlMeasuredSpeed * 100 / desiredSpeed ;//getting feedback value from ADC
-    HD44780_GotoXY(0,0);
-    sprintf(stringBuffer,"%03d prcnt stp",outputPercentage);
-    HD44780_PutStr(stringBuffer);      	                
-    HD44780_GotoXY(0,1);
-    sprintf(stringBuffer,"@ %03lu dutyCyc",TIM1->CCR1 * 100 / 65500);
-    HD44780_PutStr(stringBuffer);			
-  }
-  LCDDelay++;
-  
-  //printf("TIM1->CCR1 = %lu\n", TIM1->CCR1);	
-}
-ADD_TASK(controlTask, controlInit, NULL, 20, "DC motor control task")
-
-/*************Commands****************************/
-
-ParserReturnVal_t CmdSetInput(int mode) {
-
-  if (mode != CMD_INTERACTIVE) return CmdReturnOk;
-  fetch_uint16_arg(&desiredSpeed);		//inputting desired speed
-  TIM1->CR1 &= ~TIM_CR1_CEN;    //stopping timer	
-  TIM1->CCR1 = desiredSpeed * 65500 / 1738;
-  TIM1->CR1 |= TIM_CR1_CEN;	//resuming timer
-  HAL_ADC_Start_IT(&hadc);	//start ADC
-  //printf("TIM1->CCR1 = %lu\n", TIM1->CCR1);
-  return CmdReturnOk;
-}
-ADD_CMD("SetInput", CmdSetInput, "		Sets desired speed 0 - 1738")
-
-/***************************ISR****************************/
-
-void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc1)
-    {   
-	
-	int32_t newValue;
-        if(hadc1->Instance == ADC1){
-  	  controlMeasuredSpeed = HAL_ADC_GetValue(&hadc);
-	  
-	  //PID controller code
-	  if(controlMeasuredSpeed > desiredSpeed){
-	    errorValue = controlMeasuredSpeed - desiredSpeed;
-	  }else{
-	    errorValue = desiredSpeed - controlMeasuredSpeed;
-	  }
-
-	  errorValue = errorValue * Kp;
-
-	  if(desiredSpeed > controlMeasuredSpeed){
-	    newValue = errorValue + desiredSpeed;
-	  }else{
-	    newValue = desiredSpeed - errorValue;
-	  }
-	  //setting limits for the new value
-	  if(newValue > 1738){
-	    newValue = 1738;
-	  }else if(newValue < 0){
-	    newValue = 0;
-	  }
-	  testNewValue = newValue;
-	  if(errorValue != 0){
-	    TIM1->CR1 &= ~TIM_CR1_CEN;  //stopping timer	
-	    TIM1->CCR1 =  newValue * 65500 / 1738;
-	    TIM1->CR1 |= TIM_CR1_CEN;	//resuming timer
-	  }
-	}
-    }
-void ADC1_IRQHandler(){
-	HAL_ADC_IRQHandler(&hadc); 
+  HD44780_Init();	//initializing LCD	
 }
